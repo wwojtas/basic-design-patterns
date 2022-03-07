e4fadb8463fca67ad37166eea17fff187148ab0d
wwojtas <wojciech.wus.programista@gmail.com>
2022-03-07 15:09:21 +0100

    Memento

---
+++ src/main/java/co/Patterns/Memento/Main.java
@@ -0,0 +1,27 @@
+package main.java.co.Patterns.Memento;
+
+import main.java.co.Patterns.Memento.smart_app.SmartApp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SmartApp smartApp = new SmartApp();
+        smartApp.setVersion(1.0);
+        System.out.println(smartApp);
+
+        smartApp.setVersion(1.1);
+        System.out.println(smartApp);
+
+        smartApp.getVersionList().add(1.1);
+
+        smartApp.setVersion(1.2);
+        System.out.println(smartApp);
+
+        smartApp.setVersion(2.0);
+        System.out.println(smartApp);
+
+        smartApp.setVersion(smartApp.getVersionList().get(0));
+        System.out.println(smartApp);
+
+    }
+}
